9920954b0ebaa4556d846533d424e7364efb67db
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-10-08 20:12:11 +0200

    added custom malloc

--- wb_main.c
+++ wb_main.c
@@ -3,20 +3,74 @@
 
 #include "pub_tool_basics.h"
 #include "pub_tool_tooliface.h"
+#include "pub_tool_replacemalloc.h"
 
+///////////////// mallocs ///////////////////
+
+static void* wb_malloc ( ThreadId tid, SizeT szB )
+{
+	return VG_(cli_malloc)(VG_(clo_alignment), szB);
+}
+
+static void* wb___builtin_new ( ThreadId tid, SizeT szB )
+{
+	return VG_(cli_malloc)(VG_(clo_alignment), szB);
+}
+
+static void* wb___builtin_vec_new ( ThreadId tid, SizeT szB )
+{
+	return VG_(cli_malloc)(VG_(clo_alignment), szB);
+}
+
+static void* wb_calloc ( ThreadId tid, SizeT m, SizeT szB )
+{
+	return VG_(cli_malloc)(VG_(clo_alignment), m*szB);
+}
+
+static void *wb_memalign ( ThreadId tid, SizeT alignB, SizeT szB )
+{
+	return VG_(cli_malloc)(alignB, szB);
+}
+
+static void wb_free ( ThreadId tid __attribute__((unused)), void* p )
+{
+   VG_(cli_free)(p);
+}
+
+static void wb___builtin_delete ( ThreadId tid, void* p )
+{
+   VG_(cli_free)(p);
+}
+
+static void wb___builtin_vec_delete ( ThreadId tid, void* p )
+{
+   VG_(cli_free)(p);
+}
+
+static void* wb_realloc ( ThreadId tid, void* p_old, SizeT new_szB )
+{
+	return NULL;
+}
+
+static SizeT wb_malloc_usable_size ( ThreadId tid, void* p )
+{
+	return VG_(cli_malloc_usable_size)(p);
+}                                                            
+
+///////////////////////// inits ///////////////////////////////
 static void wb_post_clo_init(void)
 {
 }
 
 static
 IRSB* wb_instrument ( VgCallbackClosure* closure,
-                      IRSB* bb,
-                      const VexGuestLayout* layout, 
-                      const VexGuestExtents* vge,
-                      const VexArchInfo* archinfo_host,
-                      IRType gWordTy, IRType hWordTy )
+		IRSB* bb,
+		const VexGuestLayout* layout, 
+		const VexGuestExtents* vge,
+		const VexArchInfo* archinfo_host,
+		IRType gWordTy, IRType hWordTy )
 {
-    return bb;
+	return bb;
 }
 
 static void wb_fini(Int exitcode)
@@ -25,20 +79,33 @@ static void wb_fini(Int exitcode)
 
 static void wb_pre_clo_init(void)
 {
-   VG_(details_name)            ("Whiteboard");
-   VG_(details_version)         (NULL);
-   VG_(details_description)     ("TODO");
-   VG_(details_copyright_author)(
-      "Copyright (C) 2016 Maciej Gajewski");
-   VG_(details_bug_reports_to)  (VG_BUGS_TO);
+	VG_(details_name)            ("Whiteboard");
+	VG_(details_version)         (NULL);
+	VG_(details_description)     ("TODO");
+	VG_(details_copyright_author)(
+		"Copyright (C) 2016 Maciej Gajewski");
+	VG_(details_bug_reports_to)  (VG_BUGS_TO);
 
-   VG_(details_avg_translation_sizeB) ( 275 );
+	VG_(details_avg_translation_sizeB) ( 275 );
 
-   VG_(basic_tool_funcs)        (wb_post_clo_init,
-                                 wb_instrument,
-                                 wb_fini);
+	VG_(basic_tool_funcs)        (wb_post_clo_init,
+									wb_instrument,
+									wb_fini);
 
-   /* No needs, no core events to track */
+	// needs
+	VG_(needs_var_info)();
+	VG_(needs_malloc_replacement)  (wb_malloc,
+		wb___builtin_new,
+		wb___builtin_vec_new,
+		wb_memalign,
+		wb_calloc,
+		wb_free,
+		wb___builtin_delete,
+		wb___builtin_vec_delete,
+		wb_realloc,
+		wb_malloc_usable_size,
+		0 );
+
 }
 
 VG_DETERMINE_INTERFACE_VERSION(wb_pre_clo_init)
