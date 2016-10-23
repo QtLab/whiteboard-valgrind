9a9b7a9eb1ecf2b75f9591bf138da42674fb2159
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-10-23 11:18:53 +0200

    printing every SP move makes no sense

--- wb_main.c
+++ wb_main.c
@@ -6,6 +6,7 @@
 #include "pub_tool_replacemalloc.h"
 #include "pub_tool_libcprint.h"
 #include "pub_tool_vki.h"
+#include "pub_tool_libcassert.h"
 
 /// out ////
 static VgFile* output = NULL;
@@ -16,21 +17,21 @@ static VgFile* output = NULL;
 static void* wb_malloc ( ThreadId tid, SizeT szB )
 {
 	void* addr = VG_(cli_malloc)(VG_(clo_alignment), szB);
-	VG_(fprintf)(output, "{\"action\" : \"allocation\", \"type\" : \"malloc\", \"size\" : %ull, \"addr\" : %p }\n", szB, addr);
+	VG_(fprintf)(output, "{\"action\" : \"allocation\", \"type\" : \"malloc\", \"size\" : %lu, \"addr\" : %p }\n", szB, addr);
 	return addr;
 }
 
 static void* wb___builtin_new ( ThreadId tid, SizeT szB )
 {
 	void* addr = VG_(cli_malloc)(VG_(clo_alignment), szB);
-	VG_(fprintf)(output, "{\"action\" : \"allocation\", \"type\" : \"new\", \"size\" : %ull, \"addr\" : %p }\n", szB, addr);
+	VG_(fprintf)(output, "{\"action\" : \"allocation\", \"type\" : \"new\", \"size\" : %lu, \"addr\" : %p }\n", szB, addr);
 	return addr;
 }
 
 static void* wb___builtin_vec_new ( ThreadId tid, SizeT szB )
 {
 	void* addr = VG_(cli_malloc)(VG_(clo_alignment), szB);
-	VG_(fprintf)(output, "{\"action\" : \"allocation\", \"type\" : \"new[]\", \"size\" : %ull, \"addr\" : %p }\n", szB, addr);
+	VG_(fprintf)(output, "{\"action\" : \"allocation\", \"type\" : \"new[]\", \"size\" : %lu, \"addr\" : %p }\n", szB, addr);
 	return addr;
 }
 
@@ -70,12 +71,28 @@ static void* wb_realloc ( ThreadId tid, void* p_old, SizeT new_szB )
 static SizeT wb_malloc_usable_size ( ThreadId tid, void* p )
 {
 	return VG_(cli_malloc_usable_size)(p);
-}                                                            
+}                               
+
+/////////////////////////////// stack ///////////////////////////
+// this guys are called a lot!
+static void wb_new_mem_stack(Addr a, SizeT len)
+{
+	//VG_(fprintf)(output, "{\"action\" : \"new-stack\", \"size\" : %lu, \"addr\" : %p }\n", len, (void*)a);
+}
+
+static void wb_die_mem_stack(Addr a, SizeT len)
+{
+	//VG_(fprintf)(output, "{\"action\" : \"die-stack\", \"size\" : %lu, \"addr\" : %p }\n", len, (void*)a);
+}
 
 ///////////////////////// inits ///////////////////////////////
 static void wb_post_clo_init(void)
 {
 	output = VG_(fopen)("/dev/stdout", VKI_O_CREAT|VKI_O_TRUNC|VKI_O_WRONLY, VKI_S_IRUSR|VKI_S_IWUSR);
+	if (output == NULL)
+	{
+		VG_(tool_panic)("unable to open output file");
+	}
 }
 
 static
@@ -111,6 +128,8 @@ static void wb_pre_clo_init(void)
 
 	// needs
 	VG_(needs_var_info)();
+	VG_(needs_libc_freeres)();	// do i
+	VG_(needs_cxx_freeres)();	// really need this?
 	VG_(needs_malloc_replacement)  (wb_malloc,
 		wb___builtin_new,
 		wb___builtin_vec_new,
@@ -122,6 +141,8 @@ static void wb_pre_clo_init(void)
 		wb_realloc,
 		wb_malloc_usable_size,
 		0 );
+	VG_(track_new_mem_stack)(wb_new_mem_stack);
+	VG_(track_die_mem_stack)(wb_die_mem_stack);
 
 }
 
