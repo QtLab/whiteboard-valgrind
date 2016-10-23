4582956f2c4a437b84d3532cf1b28180d31e706e
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-10-23 20:51:05 +0200

    emmiting unconditional stores

--- wb_instrumentation.c
+++ wb_instrumentation.c
@@ -11,14 +11,62 @@
 #include "pub_tool_libcbase.h"
 #include "pub_tool_debuginfo.h"
 #include "pub_tool_mallocfree.h"
+#include "pub_tool_libcassert.h"
+
 
 Bool wb_above_main = False;
 Bool wb_inside_user_code = False;
+
+// Mem access tracking ////////////////////
+
+struct MemOp {
+    Addr addr;
+    SizeT size;
+};
+
+static struct MemOp last_write = {0, 0};
+
+static void flush_mem_ops(void)
+{
+    if (last_write.size > 0 ) {
+        VG_(fprintf)(wb_output, "{\"action\" : \"store\", \"addr\" : %p, \"size\" : %lu}\n", (void*)last_write.addr, last_write.size);
+    }
+    last_write.size = 0;
+    last_write.addr = 0;
+}
+
+static void wb_on_store(Addr addr, SizeT size)
+{
+    if (wb_above_main) {
+        
+        // first write since flush
+        if (last_write.addr == 0) {
+            last_write.addr = addr;
+            last_write.size = size;
+        }
+        // write imemdiately after the last one
+        else if ((last_write.addr + last_write.size) == addr ) {
+            last_write.size += size;
+        }
+        // write immediately preceeding the last one
+        else if ((addr + size) == last_write.addr) {
+            last_write.addr = addr;
+            last_write.size += size;
+        }
+        // another region, need to report separately
+        else {
+            flush_mem_ops();
+            last_write.addr = addr;
+            last_write.size = size;
+        }
+    }
+}
 
 
 static int superblock_counter = 0;
 static int imark_counter = 0;
 
+// source and line tracking /////////////////////
 static HChar* main_source_dir = NULL;
 
 struct LastLine {
@@ -33,6 +81,7 @@ static void flush_last_line(void)
 {
     if (last_line.linenum > 0) {
         flush_stack();
+        flush_mem_ops();
         VG_(fprintf)(wb_output, "{\"action\" : \"line-step\", \"file\" : \"%s\", \"line\" : %u, \"dir\" : \"%s\"}\n", last_line.filename, last_line.linenum, last_line.dirname);
     }
 }
@@ -80,6 +129,7 @@ static void wb_on_instruction(Addr addr)
     }
 }
 
+
 IRSB* wb_instrument ( VgCallbackClosure* closure,
         IRSB* sbIn, // superblock (single entry, multiple exit code sequence)
         const VexGuestLayout* layout, 
@@ -101,16 +151,40 @@ IRSB* wb_instrument ( VgCallbackClosure* closure,
     // copy all the other statements
     for (/* reused */; i < sbIn->stmts_used; i++) {
         IRStmt* st = sbIn->stmts[i];
+        
+        // IMark
         if (st->tag == Ist_IMark) {
             imark_counter ++;
 
             // instrument every IMark
             IRDirty* di = unsafeIRDirty_0_N(
-                    0, "wb_on_instruction", 
+                    0, "wb_on_instruction",  // TODO should this be '1'?
                     VG_(fnptr_to_fnentry)( &wb_on_instruction ), 
                     mkIRExprVec_1(mkIRExpr_HWord( st->Ist.IMark.addr )) );
             addStmtToIRSB( sbOut, IRStmt_Dirty(di) );
             
+        }
+        // Store
+        else if (st->tag == Ist_Store) {
+            
+            IRExpr* data = st->Ist.Store.data;
+            IRExpr* addr = st->Ist.Store.addr;
+            IRType type = typeOfIRExpr(sbIn->tyenv, data);
+            Int dsize = sizeofIRType(type);
+            
+//             addEvent_Dw( sbOut, st->Ist.Store.addr,
+//                         sizeofIRType(type) );
+            tl_assert(isIRAtom(addr));
+            tl_assert(dsize >= 1 && dsize <= 512);
+            
+            IRExpr** argv =  mkIRExprVec_2(addr, mkIRExpr_HWord(dsize));
+            IRDirty* di   = unsafeIRDirty_0_N(
+                    2, // params 
+                    "wb_on_store",
+                    VG_(fnptr_to_fnentry)(&wb_on_store),
+                    argv );
+
+            addStmtToIRSB( sbOut, IRStmt_Dirty(di) );
         }
         
         addStmtToIRSB( sbOut, st );
